9764f8d715dfff59b36bdabf05eee267d9514830
liangwenwei <liangwenwei@163.com>
2024-04-24 16:58:13 +0800

    django_bootstrap5样式的学习

--- ll_project/learning_logs/forms.py
+++ ll_project/learning_logs/forms.py
@@ -5,11 +5,12 @@ class TopicForm(forms.ModelForm):
     class Meta:
         model=Topic
         fields=['text']
-        labels={'text':'主题'}
+        labels={'text':''}
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model=Entry
         fields='__all__'
         labels={'text':''}
-        widgets={'text':forms.Textarea(attrs={'cols':80})}
+        # 修改渲染表单的属性
+        widgets={'text':forms.Textarea(attrs={'cols':80}),'topic':forms.Select(attrs={'disabled':True})}
